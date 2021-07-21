1a972988d81e1f04726959a4eb1e9b25af03aaeb
Rui Xian <tayrx9988@gmail.com>
2021-07-21 14:55:08 +0800

    add cart reducer to store

--- Redux/store.ts
+++ Redux/store.ts
@@ -1,5 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit'
+import cartReducer from './features/CartSlice'
 
 export default configureStore({
-  reducer: {}
+  reducer: {
+      cart: cartReducer,
+  }
 })
